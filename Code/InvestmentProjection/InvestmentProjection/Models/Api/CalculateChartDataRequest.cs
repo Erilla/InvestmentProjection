fe65746ceead9449cbd52d64007703849c79eb94
Ryan Wong <me@ryanwong.uk>
2021-06-05 05:31:19 +0100

    Implemented input boxes and ajax call to the GET endpoint

--- Code/InvestmentProjection/InvestmentProjection/Models/Api/CalculateChartDataRequest.cs
+++ Code/InvestmentProjection/InvestmentProjection/Models/Api/CalculateChartDataRequest.cs
@@ -5,18 +5,23 @@ namespace InvestmentProjection.Models.Api
     public class CalculateChartDataRequest
     {
         [Required]
+        [Display(Name = "Lump Sum Investment (£)")]
         public decimal LumpSumInvestment { get; init; }
 
         [Required]
+        [Display(Name = "Monthly Investment (£)")]
         public decimal MonthlyInvestment { get; init; }
 
         [Required]
+        [Display(Name = "Target Value (£)")]
         public decimal TargetValue { get; init; }
 
         [Required]
+        [Display(Name = "Timescale (Years)")]
         public int Timescale { get; init; }
 
         [Required]
+        [Display(Name = "Risk Level")]
         public RiskLevel RiskLevel { get; init; }
     }
 }
